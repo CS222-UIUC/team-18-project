d283cb4a25d8b44750da0e8d374ac5fe7420af27
saumya <saumyaa4@illinois.edu>
2025-03-07 14:27:09 -0600

    fixed the title

--- FrontEnd-cs222-project/src/app/components/Title.js
+++ FrontEnd-cs222-project/src/app/components/Title.js
@@ -4,7 +4,7 @@ import React from 'react'
 
 function Title() {
     return (<header style={styles.header}>
-    <h1> UIUC Minor Recommender</h1>
+    <h1 style={styles.title}>UIUC Minor Recommender</h1>
     </header>);
 }
 
@@ -14,6 +14,16 @@ const styles = {
         color: 'black',
         padding: '10px 20px',
         textAlign: 'center',
+        boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
+        borderRadius: '8px',
+    },
+    title: {
+        color: 'black',
+        fontSize: '2.5rem',
+        margin: '0',
+        fontWeight: 'bold',
+        fontFamily: 'Merriweather, sans-serif',
+        letterSpacing: '1.5px',
     },
 };
 
