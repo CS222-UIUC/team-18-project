47d00ad8bbd8ef2ced78ac654a7040b576f49d03
MNathanM <mcelroy6@illinois.edu>
2025-04-12 17:28:31 -0500

    Created MinorPercentList component to display percentages on the second page (statically)

--- FrontEnd-cs222-project/src/app/pages/secondary.tsx
+++ FrontEnd-cs222-project/src/app/pages/secondary.tsx
@@ -8,6 +8,7 @@ import LinkButton from "../components/LinkButton.js";
 import React, { useEffect, useState } from 'react'; 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import MinorPercentList from "../components/MinorPercentList.js";
 
 
 export default function Secondary({refresh = 0}) {
@@ -24,12 +25,13 @@ export default function Secondary({refresh = 0}) {
     }
   }, []);
 
+  const minors = ['Business', 'Business Analytics', 'Technology & Management', 'Computer Science', 'Math', 'Data Science', 'Economics', 'Statistics', 'Spanish', 'Physics'];
+  const percentages = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90];
+
   return (<div style={{color : 'black'}}>
     <Title/> <Subtitle string={"Here Is Your Recommended Minor Data:"} />
-    <div>
-    Second Page
-    </div>
-  <button onClick={goToHomePage} style={{color: 'black', marginLeft: '10px', marginTop: '430px'}}> Go To Home Page</button>
+    <MinorPercentList minors={minors} percentages={percentages}/>
+  <button onClick={goToHomePage} style={{color: 'black', marginLeft: '10px', marginTop: '200px'}}> Go To Home Page</button>
    </div>
   )
 }
