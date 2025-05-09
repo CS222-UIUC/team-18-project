3e8d40dd17bac4aaf2474c182281d028d4bfedb8
MNathanM <mcelroy6@illinois.edu>
2025-05-09 13:34:04 -0500

    Added in className prop to Dropdown and Major Dropdown

--- FrontEnd-cs222-project/src/app/components/majorDropdown.js
+++ FrontEnd-cs222-project/src/app/components/majorDropdown.js
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 
-function MajorDropdown({ title, options, handleSelect }) {
+function MajorDropdown({ title, options, handleSelect, className }) {
   const [open, setOpen] = useState(false);
   const [selected, setSelected] = useState(null);
 
